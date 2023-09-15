88e8591d84efcb794891d87ead2a7bfecd2485c9
Mathieu-COSYNS <64072917+Mathieu-COSYNS@users.noreply.github.com>
2023-09-15 18:59:39 +0200

    Fix scale https://github.com/soldair/node-qrcode/pull/247/files

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,16 +1,31 @@
+import { type QRCodeRenderersOptions } from 'qrcode';
 import { expect, it } from 'vitest';
 
 import * as QRCodeCaption from '../src/index';
 
-it.each`
-  text     | caption      | options                                       | snapshot
-  ${'new'} | ${undefined} | ${{}}                                         | ${1}
-  ${'new'} | ${undefined} | ${{ color: { dark: '#f00' } }}                | ${2}
-  ${'new'} | ${undefined} | ${{ color: { light: '#f00' } }}               | ${3}
-  ${'new'} | ${undefined} | ${{ color: { dark: '#f00', light: '#0f0' } }} | ${4}
-  ${'new'} | ${undefined} | ${{ color: { light: '#00000000' } }}          | ${5}
-  ${'new'} | ${undefined} | ${{ width: 100 }}                             | ${6}
-`('should render SVG $snapshot', ({ text, caption, options, snapshot }) => {
-  const result = QRCodeCaption.toSVG(text, caption, options);
-  expect(result).toMatchFileSnapshot(`__snapshots__/${snapshot}.svg`);
-});
+it.each([
+  { text: 'new', snapshot: 1 },
+  { text: 'new', options: { color: { dark: '#f00' } }, snapshot: 2 },
+  { text: 'new', options: { color: { light: '#f00' } }, snapshot: 3 },
+  { text: 'new', options: { color: { dark: '#f00', light: '#0f0' } }, snapshot: 4 },
+  { text: 'new', options: { color: { light: '#00000000' } }, snapshot: 5 },
+  { text: 'new', options: { width: 100 }, snapshot: 6 },
+  { text: 'http://www.google.com', options: { errorCorrectionLevel: 'H' as const, scale: 1 }, snapshot: 7 },
+  { text: 'http://www.google.com', options: { errorCorrectionLevel: 'H' as const, scale: 2 }, snapshot: 8 },
+])(
+  'should render SVG $snapshot',
+  ({
+    text,
+    caption,
+    options,
+    snapshot,
+  }: {
+    text: string;
+    caption?: string;
+    options?: QRCodeRenderersOptions;
+    snapshot: number;
+  }) => {
+    const result = QRCodeCaption.toSVG(text, caption, options);
+    expect(result).toMatchFileSnapshot(`__snapshots__/${snapshot}.svg`);
+  },
+);
