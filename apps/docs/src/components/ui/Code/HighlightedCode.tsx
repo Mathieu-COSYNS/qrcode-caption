28afee4cc09f3e99aaa5a6e53927d75af5b2c593
Mathieu-COSYNS <64072917+Mathieu-COSYNS@users.noreply.github.com>
2026-05-03 21:36:55 +0200

    pref(docs): replace shiki by sugar-high to improve code preview preformance

--- apps/docs/src/components/ui/Code/HighlightedCode.tsx
+++ apps/docs/src/components/ui/Code/HighlightedCode.tsx
@@ -1,25 +1,10 @@
-import { createHighlighterCore } from "shiki/core";
-import { createJavaScriptRegexEngine } from "shiki/engine/javascript";
-
-const highlighter = await createHighlighterCore({
-  themes: [import("shiki/themes/github-light.mjs"), import("shiki/themes/github-dark.mjs")],
-  langs: [import("shiki/langs/javascript.mjs")],
-  engine: createJavaScriptRegexEngine(),
-});
+import { highlight } from "sugar-high";
 
 export const HighlightedCode = ({ code }: { code: string }) => {
   return (
-    <div
-      dangerouslySetInnerHTML={{
-        __html: highlighter.codeToHtml(code, {
-          lang: "javascript",
-          themes: {
-            light: "github-light",
-            dark: "github-dark",
-          },
-        }),
-      }}
-    />
+    <pre>
+      <code dangerouslySetInnerHTML={{ __html: highlight(code) }} />
+    </pre>
   );
 };
 
